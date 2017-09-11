b26896c99645d40cb15831f2872a36475a64cb9f
Todd Utterback <todd.j.utterback@gmail.com>
2017-09-11 13:12:01 -0600

    pausing to work on reflect.io with Dal. halfway through hooks chapter

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -3,6 +3,15 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     var bands = this.modelFor('bands');
-    return bands.findBy('slug', params.slug); // params.slug is now 'pearl-jam'
+    return bands.findBy('slug', params.slug);
+  },
+
+  afterModel: function(band) {
+  	var description = band.get('description');
+  	if (Ember.isEmpty(description)) {
+  		this.transitionTo('bands.band.songs');
+  	} else {
+  		this.transitionTo('bands.band.details');
+  	}
   }
 });
