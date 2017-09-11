c8bd9c793dc40cf6a4f2904dfa8b75cb6e58e886
Todd Utterback <todd.j.utterback@gmail.com>
2017-09-11 12:10:16 -0600

    end of Controllers Chapter. App works

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,30 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	songCreationStarted: false,
+  songCreationStarted: false,
 
-	noSongs: Ember.computed('model.songs.length', function() {
-		return this.get('model.songs.length') === 0;
-	}),
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  }),
 
-	canCreateSong: Ember.computed('songCreatedStarted', 'model.songs.length', function() {
-		return this.get(;songCreationStarted) || this.get('model.songs.length');
-	}),
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
 
-	actions: {
-		enableSongCreation: function() {
-			this.set('songCreationStarted', true);
-		},
+  actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
 
-		updateRating: function() {
-			var song = params.item, 
-				rating = params.rating;
+    updateRating: function(params) {
+      var song = params.item,
+          rating = params.rating;
 
-			song.set('rating', rating);
-		}
-	},
-
-	isAddButtonDisabled: Ember.computed('title', function() {
-		return Ember.isEmpty(this.get('title'));
-	})
+      song.set('rating', rating);
+    }
+  }
 });
